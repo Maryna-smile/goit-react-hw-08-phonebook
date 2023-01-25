8ab5b3580d511711fa258dcf0fae556921c7578f
Maryna-smile <maryna.naidych@gmail.com>
2023-01-25 15:43:10 +0200

    add normal validation

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -67,7 +67,7 @@ export default function ContactForm() {
           inputProps={{
             inputMode: 'numeric',
             pattern:
-              /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/,
+            "/^+380d{3}d{2}d{2}d{2}$/",
             title:
               'Phone number must be digits and can contain spaces, dashes, parentheses and can start with +',
           }}
